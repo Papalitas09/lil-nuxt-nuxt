79ae343ed85f0db84e83acfae6d2051d88828f5d
Mr.Papalitas <miaw.raja@gmail.com>
2025-10-20 23:39:37 +0700

    apdet 0

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
-   <layout class="overflow-x-hidden"  >
-      <HomeComponentsSummary />
+   <layout class="overflow-x-hidden">
+      <HomeComponentsSummary  />
       <HomeComponentsRescom />
       <HomeComponentsContact />
    </layout>
 </template>
 <script lang="ts" setup>
 import layout from '~/layout/layout.vue';
+import gsap from 'gsap';
+// gsap.fromTo("mama", { opacity: 0 }, { opacity: 0.5, duration: 1 });
 </script>
